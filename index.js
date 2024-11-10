a086b3919cbaf375acf37321c7ebda2f3965c3c6
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2024-11-10 00:41:05 +0000

    Implemented speech to text

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-webgazer.setGazeListener(function(data, elapsedTime) {
-	if (data == null) {
-		return;
-	}
-	var xprediction = data.x; //these x coordinates are relative to the viewport
-	var yprediction = data.y; //these y coordinates are relative to the viewport
-	console.log(data); //elapsed time is based on time since begin was called
+let recorder;
+let recordedAudio = [];
 
-    document.getElementById("coordinates").innerHTML = `${xprediction}, ${yprediction}`
+async function setup() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true
+    })
 
-}).begin();
+    recorder = new MediaRecorder(stream);
+
+    recorder.ondataavailable(ev => {
+        recordedAudio.push(ev.data);
+    })
+}
+
+async function startRecording() {
+    recorder.start();
+}
+
+async function stopRecording() {
+    recorder.stop();
+}
+
+function performSpeechRecognition() {
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = 'en-US';
+
+    recognition.onstart = () => {
+        document.getElementById("recording-button").setAttribute("src", "./recording.svg");
+    };
+
+    recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        document.getElementById("results").innerHTML = `You said: "${transcript}"`
+    };
+
+    recognition.onend = () => {
+        document.getElementById("recording-button").setAttribute("src", "./mic.svg");
+    };
+
+    document.getElementById("recording-button").addEventListener('click', () => {
+        recognition.start();
+    });
+}
+
+window.onload = () => {
+    performSpeechRecognition();
+}
